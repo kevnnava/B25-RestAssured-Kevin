8c8e9a1961827aef09f9bd5490dc41c8f99b5cb4
kevinnava <kevin.nava00@gmail.com>
2022-06-04 23:19:59 -0500

    Day 2 API

---
+++ src/test/java/com/cydeo/day2/SpartanNegativeGetRequest.java
@@ -0,0 +1,48 @@
+package com.cydeo.day2;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SpartanNegativeGetRequest {
+
+    // BeforeAll is the same thing as beforeClass in testNG
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI = "http://18.232.174.189:8000";
+    }
+
+    @DisplayName("GET all Spartans")
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.
+                given().accept(ContentType.JSON)
+                .when().get("/api/spartans");
+
+        // prints out status code
+        System.out.println("response.statusCode() = " + response.statusCode());
+        // prints out content type
+        System.out.println("response.contentType() = " + response.contentType());
+
+        // how to test API
+        // verify status code is 200
+        Assertions.assertEquals(200, response.statusCode());
+        // verify content type is application/json
+        Assertions.assertEquals("application/json", response.contentType());
+
+
+    }
+
+    /*TASK
+    Given Accept type application/xml
+    When user send GET request to /api/spartans/10 end point
+    Then status code must be 406
+    And response Content Type must be application/xml;charset=UTF-8;
+    */
+
+}
